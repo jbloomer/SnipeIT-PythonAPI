3534071fae55b826203ed73dc254e8f002247e02
jlbloomer <jared.bloomer@coxautoinc.com>
2019-06-05 10:33:52 -0400

    Updating to include long description

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-with open("README.md","r") as fh:
+with open("README.rst,"r") as fh:
 	long_description = fh.read()
 	
 setup(name='snipeit',
       version='1.1',
-	  long_description="""""",
+	  long_description=long_description,
+      long_description_content_type="text/markdown",
       description=("Python library to access the SnipeIT API"),
       url='https://github.com/jbloomer/SnipeIT-PythonAPI',
       author='Jared Bloomer (Cox Automotive Inc.)',
